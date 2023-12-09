d08ff98292a0d3bdd2225653df42af672c460024
Manatsawin Hanmongkolchai <git@whs.in.th>
2023-12-09 12:44:07 +0700

    Day 9

---
+++ 9/Aoc9.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+public class Aoc9 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in).useDelimiter("\n");
+        int accum = 0;
+        while (scan.hasNext()) {
+            String history = scan.next();
+            accum += solve2(history);
+        }
+        System.out.println(accum);
+    }
+
+    private static int solve1(String history) {
+        int[] input = Arrays.stream(history.split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        ArrayList<int[]> historyTiers = new ArrayList<>();
+        historyTiers.add(input);
+
+        int[] current = input;
+        while(!isAllZero(current)){
+            System.out.println(Arrays.toString(current));
+            current = diff(current);
+            historyTiers.add(current);
+        }
+
+        int nextValue = 0;
+        for (int i = historyTiers.size() - 2; i >= 0; i--) {
+            int[] currentTier = historyTiers.get(i);
+            int lastValue = currentTier[currentTier.length - 1];
+            nextValue = lastValue + nextValue;
+        }
+
+        return nextValue;
+    }
+
+    private static int solve2(String history) {
+        int[] input = Arrays.stream(history.split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        ArrayList<int[]> historyTiers = new ArrayList<>();
+        historyTiers.add(input);
+
+        int[] current = input;
+        while(!isAllZero(current)){
+            current = diff(current);
+            historyTiers.add(current);
+        }
+
+        int nextValue = 0;
+        for (int i = historyTiers.size() - 2; i >= 0; i--) {
+            int[] currentTier = historyTiers.get(i);
+            int firstValue = currentTier[0];
+            // lastNextValue = firstValue - nextValue
+            // -nextValue = lastNextValue - firstValue
+            // nextValue = firstValue - lastNextValue
+            System.out.printf("Tier %d, %d - %d\n", i, firstValue, nextValue);
+            nextValue = firstValue - nextValue;
+        }
+        System.out.printf("Input: %s, out = %d\n", Arrays.toString(input), nextValue);
+
+        return nextValue;
+    }
+
+    private static int[] diff(int[] input) {
+        int[] out = new int[input.length - 1];
+        for(int i = 1; i < input.length; i++){
+            out[i-1] = input[i] - input[i-1];
+        }
+        return out;
+    }
+
+    private static boolean isAllZero(int[] input) {
+        return Arrays.stream(input).allMatch(i -> i == 0);
+    }
+}
